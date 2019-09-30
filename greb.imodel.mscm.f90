aa6a1512dfba0fb0e09d9eb0c580eeba4565df0e
YMI33 <zhiang.xie@monash.edu>
2019-09-30 20:07:30 +1000

    leapforg final version

--- greb.imodel.mscm.f90
+++ greb.imodel.mscm.f90
@@ -9,37 +9,37 @@ program ice_sheet_model
   integer, parameter :: ndt_yr    = ndays_yr*ndt_days        ! number of timesteps per day
   real               :: dt        = ndt_yr/2           ! time step [s]
   integer            :: ireal     = 4
-  integer            :: j, irec, nstep_end
+  integer            :: j, irec, nstep_end,i 
   real,parameter     :: pi        = 3.1416 
   real,parameter     :: dx        = 2*pi/(xdim)
-  real, dimension(xdim) :: iceH_ini, ice_H2, ice_H1, ice_H0, ice_vx, diceH_crcl
+  real, dimension(xdim,ydim) :: iceH_ini, ice_H2, ice_H1, ice_H0, ice_vx, diceH_crcl
 
-  nstep_end = 96*100
+  nstep_end = 96*10
  
-  open(301,file='ice_scheme_test.bin',ACCESS='DIRECT',FORM='UNFORMATTED', RECL=ireal*xdim)
+  open(301,file='ice_scheme_test.bin',ACCESS='DIRECT',FORM='UNFORMATTED', RECL=ireal*xdim*ydim)
   ice_vx = dx/ndt_yr
   do j = 1,xdim
-      iceH_ini(j) = exp(-dx*(j-48)**2)
+  do i = 1,ydim
+      iceH_ini(j,i) = exp(-dx*(j-48)**2)
+  end do
   end do
   ice_H1 = iceH_ini
   ice_H2 = ice_H1
 
   do irec = 1,nstep_end
+  do i = 1,ydim
       j = 1
-      diceH_crcl(j) = ice_vx(j+1)*ice_H1(j+1) - ice_vx(xdim)*ice_H1(xdim)
+      diceH_crcl(j,i) = ice_vx(j+1,i)*ice_H1(j+1,i) - ice_vx(xdim,i)*ice_H1(xdim,i)
       j = xdim
-      diceH_crcl(j) = ice_vx(1)*ice_H1(1) - ice_vx(j-1)*ice_H1(j-1)
+      diceH_crcl(j,i) = ice_vx(1,i)*ice_H1(1,i) - ice_vx(j-1,i)*ice_H1(j-1,i)
       do j = 2, xdim-1
-          diceH_crcl(j) = ice_vx(j+1)*ice_H1(j+1) - ice_vx(j-1)*ice_H1(j-1) 
+          diceH_crcl(j,i) = ice_vx(j+1,i)*ice_H1(j+1,i) - ice_vx(j-1,i)*ice_H1(j-1,i) 
       end do
-      diceH_crcl = diceH_crcl/(2*dx)
 !      dt = -sum(diceH_crcl*ice_H2)*dx**2/(sum(diceH_crcl*diceH_crcl)*dx**2)*2.
+  end do
       dt = ndt_yr/2
-
-      do j = 1,xdim
-          ice_H0 = ice_H2 - dt*diceH_crcl
-      end do
-
+      diceH_crcl = diceH_crcl/(2*dx)
+      ice_H0 = ice_H2 - dt*diceH_crcl
   write(301,rec=irec) ice_H0
   
   if(irec<2) then
